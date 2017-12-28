8db5014481e3c9249e4ba47370c92905b727e4d4
Nikola Petkanski <nikolap04@gmail.com>
2017-12-28 16:00:43 +0200

    add border

--- scripts/window.js
+++ scripts/window.js
@@ -1,6 +1,11 @@
 define(["d3"], (d3)=>{
 
-    var render_div = d3.select("body").append("div").attr("id", "render_div");
+    var render_div = d3.select("body")
+                        .append("div")
+                        .attr("id", "render_div");
+
+    var vmargin_ = 5;
+    var hmargin_ = 5;
 
     return {
         add_instance : (id, w, h) => {
@@ -21,22 +26,40 @@ define(["d3"], (d3)=>{
 
                 var render_element = render_elements[i];
 
+                var svg_width = render_element.svg_width+2*hmargin_;
+                var svg_height = render_element.svg_height+2*vmargin_;
+
                 var svg = render_div.append("svg")
                             .attr("id", render_element.svg_id)
-                            .attr("width", render_element.svg_width)
-                            .attr("height", render_element.svg_height);
+                            .attr("width", svg_width)
+                            .attr("height", svg_height)
+                            .attr("style", "display: block;margin-left: auto;margin-right: auto");
+
+                var g = svg.append("g")
+                            .attr("transform", "translate("+hmargin_+","+vmargin_+")");
 
                 var elements = render_element.svg_elements;
 
                 for(var j = 0 ; j < elements.length ; j++) {
                     var element = elements[j];
-                    svg.append("rect")
+                    g.append("rect")
                         .attr("x", element.x())
                         .attr("y", element.y())
                         .attr("width", element.width())
                         .attr("height", element.height())
                         .attr("fill", element.color());
                 }
+
+                svg.append("rect")
+                    .attr("x", 0)
+                    .attr("y", 0)
+                    .attr("width", svg_width)
+                    .attr("height", svg_height)
+                    .attr("rx", hmargin_)
+                    .attr("ry", vmargin_)
+                    .attr("fill", "none")
+                    .attr("stroke", "rgb(127,127,127)");
+
             }
 
         }
